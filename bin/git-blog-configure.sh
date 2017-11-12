6b1c9fe9babef89e7ae0a0edc0559e360ead4c83
Alex McNamara <alex.mcnamara@gmail.com>
2017-11-12 18:34:07 -0500

    Add usage, allow running config on existing blogs without breaking social handles

--- bin/git-blog-configure.sh
+++ bin/git-blog-configure.sh
@@ -1,11 +1,32 @@
 function configure() {
-    TITLE=$(echo $@)
-    NOW=$(date +"%m%d%Y%H%M")
+    cd_base
+    CONFIG_FILE="$PWD/config.yaml"
 
-    mkdir $NOW.${TITLE/\ /_}
-    cd $_
+    if [ -e $CONFIG_FILE ] && [ grep -q "(twitter|facebook)" $CONFIG_FILE]; then
+	echo "WARNING: A config file with social handles already exists, please edit $CONFIG_FILE directly."
+    else
+	# Configure social handles
+	echo "Please enter your twitter handle:"
+	read -e twitter
+	echo "Please enter your facebook handle:"
+	read -e facebook
+	cat >> $CONFIG_FILE <<CONF
+---
+twitter: $twitter
+facebook: $facebook
+---
+CONF
+    fi
 
-    touch abstract.txt
-    touch content.md
-    touch publish.lock
+    # Initialize AWS token
+    echo "\nPlease enter your AWS access token:"
+    read -s -e token
+
+    if [ -z $token ]; then
+        echo "No token provided, please provision $PWD/.aws_token before publishing"
+    else
+        echo "Writing token to $PWD/.aws_token"
+        echo "Please be careful not to check-in or otherwise make this credential public!"
+        echo $token > .aws_token
+    fi
 }
--- bin/git-blog-helpers.sh
+++ bin/git-blog-helpers.sh
@@ -1,10 +1,23 @@
 function cd_base() {
     while [ ! -e "./.git-blog" ]; do
 	if [ $PWD == "/" ]; then
-            echo "You are not in a blog directory"
+            echo "You are not in a git-blog directory"
             exit 1
 	else
             cd ..
 	fi
     done
+}
+
+function usage() {
+    cat <<EOF
+Usage:
+  git-blog --help            This message
+  git-blog init <name>       Creates a new local blog repo
+  git-blog clone <target>    Creates a local copy of an existing published blog
+  git-blog configure         Configures global metadata (social handles, AWS credentials, etc) on an existing blog repo
+  git-blog add <title>       Creates a new blog post
+  git-blog build             Builds all static assets into public
+  git-blog publish           Copies static assets to target S3 bucket
+EOF
 }

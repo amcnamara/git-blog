6e116e9db258286c70d9014c549113d86fad404b
Alex McNamara <alex.mcnamara@gmail.com>
2017-11-13 12:00:50 -0500

    Pipe in markup content when templating output.

--- bin/git-blog-build.sh
+++ bin/git-blog-build.sh
@@ -10,29 +10,33 @@ function build() {
     rsync -a ./static/* ./public
 
     for post in $(find $CONTENT_DIR -name "*.md"); do
-	# Find the template and corresponding output path for the given post
+        # Find the template and corresponding output path for the given post
         midpath=$(dirname $post | cut -d/ -f3- -)
         template=$TEMPLATE_DIR/$midpath/index.mustache
         output=$PUBLIC_DIR/$midpath/$(basename $post | cut -d. -f1 -).html
 
 	# Ensure that the template exists for the given post
         if [ ! -e $template ]; then
-	    echo "WARNING: Cannot render post, skipping due to missing template:"
-	    echo "         post '$post'"
-	    echo "         expected template '$template'"
-	    continue
-	fi
+            echo "WARNING: Cannot render post, skipping due to missing template:"
+            echo "         post '$post'"
+            echo "         expected template '$template'"
+            continue
+        fi
 
         # Ensure that public directory and midpath subdirectories exist
         if [ ! -e ./$PUBLIC_DIR/$midpath ]; then
             mkdir -p $PUBLIC_DIR/$midpath
         fi
 
+        echo "Writing '$output'"
+
+        # TODO: Consider adding support for http://www.html-tidy.org/ on output
         cat <<METADATA | cat ./config.yaml - | mustache - $template >> $output
 ---
 $(for key in $(multimarkdown -m $post); do
   echo $key: $(multimarkdown -e=$key $post)
 done)
+content: '$(multimarkdown --snippet $post)'
 ---
 METADATA
     done

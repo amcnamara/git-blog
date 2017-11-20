c33c4f2f9a46fdf8aacfe009624fe4bd63c40103
Alex McNamara <alex.mcnamara@gmail.com>
2017-11-19 20:15:48 -0500

    Don't add newlines when reading in configuration attributes.

--- bin/_git-blog-configure.sh
+++ bin/_git-blog-configure.sh
@@ -3,12 +3,9 @@ function configure_social() {
     if grep -qE "twitter|facebook|email" $CONFIG_FILE; then
         pwarning "A config file with social handles already exists, please edit $CONFIG_FILE directly"
     else
-        pbold "Please enter your twitter handle (or leave blank):"
-        read -e twitter
-        pbold "Please enter your facebook handle (or leave blank):"
-        read -e facebook
-        pbold "Please enter your email address (or leave blank):"
-        read -e email
+        read -p "Please enter your twitter handle (or leave blank): " twitter
+        read -p "Please enter your facebook handle (or leave blank): " facebook
+        read -p "Please enter your email address (or leave blank): " email
 
         if [[ ! -z $twitter || ! -z $facebook || ! -z $email ]]; then
             cat >> $CONFIG_FILE <<CONF
@@ -27,10 +24,8 @@ function configure_upstream() {
     if grep -qE "location|region" $CONFIG_FILE; then
         pwarning "A config file with an S3 endpoint already exsits, please edit $CONFIG_FILE directly"
     else
-        pbold "Please enter the AWS region where your blog is hosted:"
-        read -e region
-        pbold "Please enter the ARN of the S3 bucket where your blog is hosted:"
-        read -e bucket
+        read -p "Please enter the AWS region where your blog is hosted: " region
+        read -p "Please enter the ARN of the S3 bucket where your blog is hosted: " bucket
 	
         if [[ ! -z $bucket || ! -z $region ]]; then
             pwarning "No bucket location or region provided, please provision $CONFIG_FILE before publishing"

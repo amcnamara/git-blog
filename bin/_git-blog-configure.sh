c6debbe42d143ff4eb4a95bfdf37c73c3a4ec7f9
Alex McNamara <alex.mcnamara@gmail.com>
2018-02-09 12:30:39 -0500

    Fix empty defaults when configuring.

--- bin/_git-blog-configure.sh
+++ bin/_git-blog-configure.sh
@@ -1,24 +1,24 @@
 function configure_social() {
     # Configure social handles
-    read -e -p "Please enter your twitter handle (or leave blank): " -i $(echo_config_attribute "twitter") twitter 2>&1
+    read -e -p "Please enter your twitter handle (or leave blank): " -i "$(echo_config_attribute 'twitter')" twitter 2>&1
     write_config_attribute "twitter" $twitter
 
-    read -e -p "Please enter your facebook handle (or leave blank): " -i $(echo_config_attribute "facebook") facebook 2>&1
+    read -e -p "Please enter your facebook handle (or leave blank): " -i "$(echo_config_attribute 'facebook')" facebook 2>&1
     write_config_attribute "facebook" $facebook
 
-    read -e -p "Please enter your email address (or leave blank): " -i $(echo_config_attribute "email") email 2>&1
+    read -e -p "Please enter your email address (or leave blank): " -i "$(echo_config_attribute 'email')" email 2>&1
     write_config_attribute "email" $email
 }
 
 function configure_upstream() {
     # Configure blog hosting metadata
-    read -e -p "Please enter the AWS region where your blog is hosted: " -i $(echo_config_attribute "region") region 2>&1
+    read -e -p "Please enter the AWS region where your blog is hosted: " -i "$(echo_config_attribute 'region')" region 2>&1
     write_config_attribute "region" $region
 
-    read -e -p "Please enter the ARN of the S3 bucket where your blog is hosted: " -i $(echo_config_attribute "bucket") bucket 2>&1
+    read -e -p "Please enter the ARN of the S3 bucket where your blog is hosted: " -i "$(echo_config_attribute 'bucket')" bucket 2>&1
     write_config_attribute "bucket" $bucket
 
-    read -e -p "Please enter the domain (including http[s]://) where your blog is hosted: " -i $(echo_config_attribute "domain") domain 2>&1
+    read -e -p "Please enter the domain (including http[s]://) where your blog is hosted: " -i "$(echo_config_attribute 'domain')" domain 2>&1
     write_config_attribute "domain" $domain
 
     if [[ -z $bucket || -z $region ]]; then

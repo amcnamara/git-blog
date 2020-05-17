2d72ee63948c7c037497da0bbccec453c2ceccf6
Alex McNamara <alex.mcnamara@gmail.com>
2020-05-16 18:32:41 -0700

    Move configuration to build script, load into home instead of project directory.

--- bin/_git-blog-constants.sh
+++ bin/_git-blog-constants.sh
@@ -5,7 +5,8 @@ WHITE='\033[0;97m'
 NOCOLOUR='\033[0m'
 
 GIT_BASEDIR=$(git rev-parse --show-toplevel 2> /dev/null)
-CONFIG_FILE=$GIT_BASEDIR/global.cfg
+CONFIG_BASEDIR=~/.git-blog
+CONFIG_FILE=$CONFIG_BASEDIR/global.cfg
 LOG_FILE=${GIT_BASEDIR:-~}/debug.log
 
 PUBLIC_DIR=$GIT_BASEDIR/public

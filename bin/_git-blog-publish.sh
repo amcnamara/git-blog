5463497e5f7467c29ea44e0b97cad2f8e32e596c
Alex McNamara <alex.mcnamara@gmail.com>
2024-12-02 14:42:00 -0800

    Add feature notes for publish step.

--- bin/_git-blog-publish.sh
+++ bin/_git-blog-publish.sh
@@ -1,15 +1,21 @@
 function publish() {
     # Ensure that all local changes have been committed
     if [[ $(git status --porcelain) ]]; then
-	perror "Cannot publish, you have uncommited local changes"
-	exit 1
+        perror "Cannot publish, you have uncommited local changes"
+        exit 1
     fi
 
+    # TODO: Write a publish datestamp into any posts which don't already
+    #       have one, this will be rendered both on the post markup as
+    #       well as the RSS feed as the file creation time isn't as
+    #       meaningful especially if a post takes a long time to write.
+    # TODO: Have a flag for draft posts which will not get datestamps,
+    #       and which are not rendered on build but are in the bundle.
+    pwarning "Publish datestamps are not yet implemented"
+
     # Fire off a build
-    # TODO: Find a better way to determine if a build has already been run and
-    #       the assets exist under public. Perhaps fingerprint with commit ID?
     build
 
-    # TODO: Upload public to S3 and CF.
+    # TODO: Upload ./public to S3 and CF.
     pwarning "Pushing to S3 is not yet implemented"
 }

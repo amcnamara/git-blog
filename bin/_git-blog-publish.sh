ea75ad5ddfcb99cbbb0801bc4421deab203f76ea
Alex McNamara <alex.mcnamara@gmail.com>
2024-12-02 20:36:35 -0800

    Inject datestamp on content publish.

--- bin/_git-blog-publish.sh
+++ bin/_git-blog-publish.sh
@@ -5,15 +5,26 @@ function publish() {
         exit 1
     fi
 
-    # TODO: Write a publish datestamp into any posts which don't already
-    #       have one, this will be rendered both on the post markup as
-    #       well as the RSS feed as the file creation time isn't as
-    #       meaningful especially if a post takes a long time to write.
+    # Write a publish datetime into any posts which don't already
+    # have one.  This will be rendered both on the post markup as
+    # well as the RSS feed as the file creation time isn't as
+    # meaningful especially if a post takes a long time to write.
+    #
     # TODO: Have a flag for draft posts which will not get datestamps,
     #       and which are not rendered on build but are in the bundle.
+    for post in `echo $(find $POST_DIR -name "*.md")`; do
+        if [[ ! $(head -n 1 $post) =~ "publish_date:" ]]; then
+            plog "Injecting publish datestamp into $post"
+            echo -e "publish_date: $(date -R)\n$(cat $post)" > $post
+        fi
+    done
+
+    git add $GIT_BASEDIR/content/posts
+    git commit -m "[git-blog publish] Injecting content timestamp."
+
     pwarning "Publish datestamps are not yet implemented"
 
-    # Fire off a build
+    # Fire off a clean build
     build
 
     # TODO: Upload ./public to S3 and CF.
